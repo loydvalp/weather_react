3b28cab5ae4ebed15b0005606eda10397f549c9f
Valencia Loyd <loyd_valencia@yahoo.com>
2020-07-11 13:36:02 -0500

    adding project

---
+++ src/Weather.js
@@ -0,0 +1,103 @@
+import React from "react";
+import "./Weather.css";
+//import Forecast from "./Forecast";
+
+export default function Weather() {
+  let weatherData = {
+    city: "Houston",
+    date: "Friday -07/03",
+    time: "12:00",
+    Temperature: "32",
+    description: "Cloudy",
+    imgUrl: "https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
+    gitUrl: "https://github.com/loydvalp/My_Weather_App_Project.git",
+    feels: "Feels like 36ºC",
+    humidity: "Humidity: 58% ",
+    wind: "Wind: 2 m/s",
+  };
+  return (
+    <div className="Weather">
+      <h1>How's the Weather?</h1>
+      <div className="search">
+        <div className="row">
+          <div className="col">
+            <form>
+              <div className="input-group">
+                <input
+                  type="search"
+                  className="form-control"
+                  placeholder="Search City..."
+                />
+                <div className="input-group-btn">
+                  <button className="btn btn-default" type="submit">
+                    <i className="fas fa-search" />
+                  </button>
+                  <button className="btn btn-default">
+                    <i className="fas fa-location-arrow" />
+                  </button>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+
+      <div className="today">
+        <div className="row">
+          <div className="col-sm-6">
+            <div className="todayWeather">
+              <div className="card text-center">
+                <div className="card-body">
+                  <h5 className="card-title">
+                    <i className="fas fa-map-pin" />
+                    <span>{weatherData.city}</span>
+                  </h5>
+                  <p className="card-text">
+                    Today| <span>{weatherData.date} </span>
+                    <br />
+                    <span> {weatherData.time} </span> <br />
+                    <span className="tempNow">
+                      <span>{weatherData.Temperature}</span>
+                      <a href="/">°C</a> |<a href="/">°F</a>
+                    </span>
+                    <br />
+                    <span>{weatherData.description}</span>
+                    <br />
+                    <br />
+                    <br />
+                    <span>{weatherData.feels} </span> <br />
+                    <span>{weatherData.humidity} </span> <br />
+                    <span>{weatherData.wind} </span>
+                  </p>
+                  <p className="card-text">
+                    <small className="text-muted">
+                      Last updated 10 mins ago
+                    </small>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-sm-6">
+            <span className="weatherIcon">
+              <img src={weatherData.imgUrl} className="card-img-top" alt="" />
+            </span>
+          </div>
+        </div>
+      </div>
+      <div class="row weather-forecast" id="forecast" />
+      <div class="author">
+        <p>
+          <small>
+            <div>
+              <a href="https://github.com/loydvalp/My_Weather_App_Project.git">
+                Open Source Coding
+              </a>
+            </div>
+            <strong>by Valencia Loyd</strong>
+          </small>
+        </p>
+      </div>
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import Weather from "./Weather";
 
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Weather />
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change

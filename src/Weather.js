9e03cbd162828fef2e4a2f4636bda6d540f6831a
Valencia Loyd <loyd_valencia@yahoo.com>
2020-07-13 17:54:30 -0500

    adding bootstrap

--- src/Weather.js
+++ src/Weather.js
@@ -16,84 +16,86 @@ export default function Weather() {
   };
   return (
     <div className="Weather">
-      <h1>How's the Weather?</h1>
-      <div className="search">
-        <div className="row">
-          <div className="col">
-            <form>
-              <div className="input-group">
-                <input
-                  type="search"
-                  className="form-control"
-                  placeholder="Search City..."
-                />
-                <div className="input-group-btn">
-                  <button className="btn btn-default" type="submit">
-                    <i className="fas fa-search" />
-                  </button>
-                  <button className="btn btn-default">
-                    <i className="fas fa-location-arrow" />
-                  </button>
+      <div className="container">
+        <h1>How's the Weather?</h1>
+        <div className="search">
+          <div className="row">
+            <div className="col">
+              <form>
+                <div className="input-group">
+                  <input
+                    type="search"
+                    className="form-control"
+                    placeholder="Search City..."
+                  />
+                  <div className="input-group-btn">
+                    <button className="btn btn-default" type="submit">
+                      <i className="fas fa-search" />
+                    </button>
+                    <button className="btn btn-default">
+                      <i className="fas fa-location-arrow" />
+                    </button>
+                  </div>
                 </div>
-              </div>
-            </form>
+              </form>
+            </div>
           </div>
         </div>
-      </div>
 
-      <div className="today">
-        <div className="row">
-          <div className="col-sm-6">
-            <div className="todayWeather">
-              <div className="card text-center">
-                <div className="card-body">
-                  <h5 className="card-title">
-                    <i className="fas fa-map-pin" />
-                    <span>{weatherData.city}</span>
-                  </h5>
-                  <p className="card-text">
-                    Today| <span>{weatherData.date} </span>
-                    <br />
-                    <span> {weatherData.time} </span> <br />
-                    <span className="tempNow">
-                      <span>{weatherData.Temperature}</span>
-                      <a href="/">°C</a> |<a href="/">°F</a>
-                    </span>
-                    <br />
-                    <span>{weatherData.description}</span>
-                    <br />
-                    <br />
-                    <br />
-                    <span>{weatherData.feels} </span> <br />
-                    <span>{weatherData.humidity} </span> <br />
-                    <span>{weatherData.wind} </span>
-                  </p>
-                  <p className="card-text">
-                    <small className="text-muted">
-                      Last updated 10 mins ago
-                    </small>
-                  </p>
+        <div className="today">
+          <div className="row">
+            <div className="col-sm-6">
+              <div className="todayWeather">
+                <div className="card text-center">
+                  <div className="card-body">
+                    <h5 className="card-title">
+                      <i className="fas fa-map-pin" />
+                      <span>{weatherData.city}</span>
+                    </h5>
+                    <p className="card-text">
+                      Today| <span>{weatherData.date} </span>
+                      <br />
+                      <span> {weatherData.time} </span> <br />
+                      <span className="tempNow">
+                        <span>{weatherData.Temperature}</span>
+                        <a href="/">°C</a> |<a href="/">°F</a>
+                      </span>
+                      <br />
+                      <span>{weatherData.description}</span>
+                      <br />
+                      <br />
+                      <br />
+                      <span>{weatherData.feels} </span> <br />
+                      <span>{weatherData.humidity} </span> <br />
+                      <span>{weatherData.wind} </span>
+                    </p>
+                    <p className="card-text">
+                      <small className="text-muted">
+                        Last updated 10 mins ago
+                      </small>
+                    </p>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-          <div className="col-sm-6">
-            <span className="weatherIcon">
-              <img src={weatherData.imgUrl} className="card-img-top" alt="" />
-            </span>
+            <div className="col-sm-6">
+              <span className="weatherIcon">
+                <img src={weatherData.imgUrl} className="card-img-top" alt="" />
+              </span>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="row weather-forecast" id="forecast" />
-      <div className="author">
-        <p>
-          <small>
-            <a href="https://github.com/loydvalp/weather_react.git">
-              Open Source Coding
-            </a>
-            <strong>by Valencia Loyd</strong>
-          </small>
-        </p>
+        <div className="row weather-forecast" id="forecast" />
+        <div className="author">
+          <p>
+            <small>
+              <a href="https://github.com/loydvalp/weather_react.git">
+                Open Source Coding
+              </a>
+              <strong> by Valencia Loyd</strong>
+            </small>
+          </p>
+        </div>
       </div>
     </div>
   );
